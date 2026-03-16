11cb5e2b37b0c8665cf3ef6080b039758c6c2099
KIAPI\kdseo <pinya982@naver.com>
2026-03-16 14:30:09 +0900

    미반영분 최신화(지도 ViewMode 설정 값 추가)

--- frontend/src/store/modules/map.module.js
+++ frontend/src/store/modules/map.module.js
@@ -4,11 +4,19 @@ const mapVars = {
 
 export const map = {
     state: () => ({
-        driverName: undefined
+        driverName: undefined,
+        viewMode: '3d',
+        bearingMode: false
     }),
     getters: {
         getMap: (state) => () => {
             return mapVars
+        },
+        getViewMode: (state) => () => {
+            return state.viewMode
+        },
+        getBearingMode: (state) => () => {
+            return state.bearingMode
         },
     },
     mutations: {
