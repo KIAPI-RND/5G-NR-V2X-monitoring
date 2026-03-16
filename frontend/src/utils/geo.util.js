9a8828a57bb54a25d7d56a87707ce0a82e893980
KIAPI\kdseo <pinya982@naver.com>
2026-03-16 14:05:07 +0900

    미반영분 최신화(calculate_heading 함수 추가)

--- frontend/src/utils/geo.util.js
+++ frontend/src/utils/geo.util.js
@@ -1,3 +1,19 @@
+export function calculate_heading(lat1, lon1, lat2, lon2) {
+  let lat1_rad = degreesToRadians(lat1)
+  let lat2_rad = degreesToRadians(lat2)
+
+  let delta_lon = degreesToRadians(lon2 - lon1)
+  let x =
+    Math.cos(lat1_rad) * Math.sin(lat2_rad) -
+    Math.sin(lat1_rad) * Math.cos(lat2_rad) * Math.cos(delta_lon)
+  let y = Math.sin(delta_lon) * Math.cos(lat2_rad)
+  let heading_rad = Math.atan2(y, x)
+
+  let heading_deg = radiansToDegrees(heading_rad)
+
+  return heading_deg
+}
+
 export function degreesToRadians(degrees) {
   return (degrees * Math.PI) / 180
 }
